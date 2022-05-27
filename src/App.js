af113e165deb445694509d3a7843c7af4f70c267
Teguh Widodo <teguhwin8@gmail.com>
2022-05-28 00:15:51 +0700

    Navbar

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ const App = () => {
 
 							{/* Pages */}
 							<Route path="/orders" element={<Orders />} />
-							<Route path="/employes" element={<Employees />} />
+							<Route path="/employees" element={<Employees />} />
 							<Route path="/customers" element={<Customers />} />
 
 							{/* Apps */}
